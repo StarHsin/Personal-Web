c872b0f0af21032ed0f0eb436100651ed79630ab
StarHsin <otzhelen2018@gmail.com>
2025-01-18 22:17:25 +0800

    feat:增加動態背景

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import DynamicBackgrounds from './DynamicBackgrounds'
+
+export default function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Welcome to My React App!</h1>
+        <DynamicBackgrounds />
+      </div>
+    </>
+  )
+}
